b48a1b5d993dffe344f8f2151b07be5f2a4da4a4
Vadix <vadim.molchanov@gmail.com>
2024-09-02 06:34:41 +0300

    почти 4 шаг

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/vadix/fullstack-javascript-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/vadix/fullstack-javascript-project-46/actions)
+
+[![asciicast](https://asciinema.org/a/Cq462QBAJq7MJJx3aDlcMCCGQ.svg)](https://asciinema.org/a/Cq462QBAJq7MJJx3aDlcMCCGQ)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
+import fs from 'fs';
+
+// Функция для загрузки и парсинга JSON-файла
+function parseJsonFile(filepath) {
+  const data = fs.readFileSync(filepath, 'utf-8');
+  return JSON.parse(data);
+}
+
+// Основная функция сравнения
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-console.log(filepath1);
-  };
+  const obj1 = parseJsonFile(filepath1);
+  const obj2 = parseJsonFile(filepath2);
+
+  const allKeys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort();
   
-  export default genDiff;
+  const result = allKeys.map((key) => {
+    if (!obj2.hasOwnProperty(key)) {
+      return `  - ${key}: ${obj1[key]}`;
+    }
+    if (!obj1.hasOwnProperty(key)) {
+      return `  + ${key}: ${obj2[key]}`;
+    }
+    if (obj1[key] !== obj2[key]) {
+      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+    }
+    return `    ${key}: ${obj1[key]}`;
+  });
+
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default genDiff;
